b5901d9b90a5319a38634d81a543b8d935312ece
Purity <shishchei@gmail.com>
2023-08-16 21:29:44 +0300

    Accessing nested elements

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,19 +1,39 @@
-export default function accessImmutableObject(object, array) {
-  if (!object || !Array.isArray(array) || array.length === 0) {
-    return undefined;
-  }
+// export default function accessImmutableObject(object, array) {
+//   if (!object || !Array.isArray(array) || array.length === 0) {
+//     return undefined;
+//   }
   
-  let currentVal = object;
+//   let currentVal = object;
 
-  for (const key of array) {
-    if (currentVal instanceof Map) {
-      currentVal = currentVal.get(key);
-    } else if (typeof currentVal === 'object' && currentVal !== null && key in value) {
-      currentVal = currentVal[key];
-    } else {
-      return undefined;
+//   for (const key of array) {
+//     if (currentVal instanceof Map) {
+//       currentVal = currentVal.get(key);
+//     } else if (typeof currentVal === 'object' && currentVal !== null && key in value) {
+//       currentVal = currentVal[key];
+//     } else {
+//       return undefined;
+//     }
+//   }
+
+//   return currentVal;
+// }
+
+export default function accessImmutableObject(object, array) {
+    if (!object || !Array.isArray(array) || array.length === 0) {
+        return undefined;
     }
-  }
 
-  return currentVal;
+    let value = object;
+    for (const key of array) {
+        if (value instanceof Map) {
+            value = value.get(key);
+        } else if (typeof value === 'object' && value !== null && key in value) {
+            value = value[key];
+        } else {
+            return undefined;
+        }
+
+    }
+    return value;
+
 }
