b9e4455d0eb2c0e7e42b80594f2456494d0aec30
Purity <shishchei@gmail.com>
2023-08-16 15:06:55 +0300

    Converting into an Immutable object using fromJS

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,19 @@
+// import { Map, fromJS } from 'immutable';
+const { fromJS } = require('immutable');
+
+function getImmutableObject(object) {
+    if (typeof object !== 'object' || object === null) {
+        throw new Error("Input must be a valid object");
+    }
+    return fromJS(object);
+}
+
+const oldMethod = {
+    fear: true,
+    smell: -1033575916.9145899,
+    wall: false,
+    thing: -914767132
+}
+
+const newMethod = getImmutableObject(oldMethod);
+console.log(newMethod);
